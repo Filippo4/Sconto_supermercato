517e2195e9416c163577080679e51c9d912462be
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:26:12 +0200

    Add project files.

---
+++ Serra/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Serra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("indicare la temperatura della serra!");
+            int temperatura = int.Parse(Console.ReadLine());
+            if (temperatura <= 5)
+            {
+                Console.WriteLine($"la temeraturea è {temperatura},perciò il livello di pericolo è alto");
+            }else if (temperatura > 5 && temperatura <= 18)
+            {
+                Console.WriteLine($"la temeraturea è {temperatura},perciò il livello di pericolo è medio");
+            }else if ( temperatura > 18)
+                Console.WriteLine($"la temeraturea è {temperatura},perciò il livello di pericolo è basso");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Supermercato/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Supermercato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("inserisci l'importo della spesa");
+            double importo = double.Parse(Console.ReadLine());
+            if (importo > 100)
+            {
+                double sconto = importo / 100 * 20;
+                double importo_finale = importo - sconto;
+                Console.WriteLine($"hai diritto ad uno sconto,l'importo scontato è {importo_finale}");
+            }
+            else
+                Console.WriteLine($"non è applicabile lo sconto,l'importo da pagare è {importo}");
+                Console.ReadLine();
+            
+        }
+    }
+}
